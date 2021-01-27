e7ebdc07b6a4f0651f85015e359769294111b2d6
lkunjumon <lkunjumon@marvell.com>
2021-01-27 13:51:53 +0530

    Adding platform support for TG48M-P

---
+++ device/delta/arm64-delta_tg48m_poe-r0/plugins/eeprom.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+    import os
+    from sonic_eeprom import eeprom_tlvinfo
+except ImportError, e:
+    raise ImportError (str(e) + "- required module not found")
+
+
+class board(eeprom_tlvinfo.TlvInfoDecoder):
+
+    def __init__(self, name, path, cpld_root, ro):
+        self.eeprom_path = "/sys/class/i2c-adapter/i2c-0/0-0057/eeprom"
+        if not os.path.exists(self.eeprom_path):
+        	os.system("echo 24c64 0x57 > /sys/class/i2c-adapter/i2c-0/new_device")
+        super(board, self).__init__(self.eeprom_path, 0, '', True)
