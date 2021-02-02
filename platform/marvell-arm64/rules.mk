d61d125d9f115eb684f6eca24625ee25bbaf632e
lkunjumon <lkunjumon@marvell.com>
2021-02-02 19:03:23 +0530

    removing pmon2.0 changes

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -1,6 +1,4 @@
 #include $(PLATFORM_PATH)/sdk.mk
-include $(PLATFORM_PATH)/platform-tg48m-p.mk
-include $(PLATFORM_PATH)/platform-tg4810m.mk
 include $(PLATFORM_PATH)/sai.mk
 include $(PLATFORM_PATH)/docker-syncd-mrvl.mk
 include $(PLATFORM_PATH)/docker-syncd-mrvl-rpc.mk
