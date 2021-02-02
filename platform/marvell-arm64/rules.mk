e019d4965c642f99dd6c373c5491aee172c1cef4
lkunjumon <lkunjumon@marvell.com>
2021-02-02 19:38:34 +0530

    Adding pmon2.0 makefiles

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -1,4 +1,5 @@
 #include $(PLATFORM_PATH)/sdk.mk
+include $(PLATFORM_PATH)/platform-tg4810m.mk
 include $(PLATFORM_PATH)/sai.mk
 include $(PLATFORM_PATH)/docker-syncd-mrvl.mk
 include $(PLATFORM_PATH)/docker-syncd-mrvl-rpc.mk
