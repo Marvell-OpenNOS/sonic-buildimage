884b1695b639f1d7131a06339f552f9e62197e8b
lkunjumon <lkunjumon@marvell.com>
2021-05-19 16:53:53 +0530

    Updated padding

--- installer/sharch_body.sh
+++ installer/sharch_body.sh
@@ -36,8 +36,8 @@ if [ "$(id -u)" = "0" ] ; then
     mount_size=$(df $tmp_dir | tail -1 | tr -s ' ' | cut -d' ' -f4)
     # Extra space for tmp
     padding=102400
-    if [ "$mount_size" -le "$((image_size + $padding))" ]; then
-        mount_size=$((((image_size + $padding)/1024/1024)+1))
+    if [ "$mount_size" -le "$((image_size + padding))" ]; then
+        mount_size=$((((image_size + padding)/1024/1024)+1))
         mount -o remount,size="${mount_size}G" -t tmpfs tmpfs-installer $tmp_dir || exit 1
     fi
 fi
